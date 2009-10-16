d7506758d3f6caf2f41cc415eaa2690e0f84da81
stafoxter <stafoxter@8dbf1cc4-b889-11de-b017-a9a2c7051f87>
2009-10-16 17:16:24 +0000

    agregue metodos auxiliares y movi a la entidad mochila los metodos q tenian mas sentido q estuviera ahi.

--- juegosistemagestion/src/juegosistemagestion/entidades/Mochila.java
+++ juegosistemagestion/src/juegosistemagestion/entidades/Mochila.java
@@ -5,6 +5,7 @@
 
 package juegosistemagestion.entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,5 +44,67 @@ public class Mochila {
     public void setObjetos(List<Objeto> objetos) {
         this.objetos = objetos;
     }
-    
+
+    //metodos con lógica --------------------------------------------------------
+
+    /**
+     * Devuelve una nueva instacia Mochila identica a la enviada por parametro
+     * @param original
+     * @return
+     */
+    public Mochila clonarMochila(Mochila original){
+        Mochila mochilaNueva = new Mochila();
+        List<Objeto> objetosNuevos = new ArrayList<Objeto>();
+        Objeto objetoNuevo;
+
+        mochilaNueva.setCapacidad(original.getCapacidad());
+
+        for (Objeto objeto : original.getObjetos()) {
+            objetoNuevo = new Objeto();
+            objetoNuevo.setBeneficio(objeto.getBeneficio());
+            objetoNuevo.setDisponible(objeto.isDisponible());
+            objetoNuevo.setVolumen(objeto.getVolumen());
+            objetoNuevo.setNombre(objeto.getNombre());
+            objetosNuevos.add(objetoNuevo);
+        }
+        mochilaNueva.setObjetos(objetosNuevos);
+        return mochilaNueva;
+    }
+
+    public boolean volumenOcupadoCorrecto(){
+        boolean resul;
+        double total= 0.0;
+        for (Objeto objeto : this.getObjetos()) {
+            if(objeto.isDisponible()){
+                total+= objeto.getVolumen();
+            }
+        }
+        if(total<= this.getCapacidad()){
+            resul = true;
+        }else{
+            resul= false;
+        }
+        return resul;
+    }
+
+    public double getBeneficioObtenido(){
+        double total= 0.0;
+        for (Objeto objeto : this.getObjetos()) {
+            if(objeto.isDisponible()){
+                total+= objeto.getBeneficio();
+            }
+        }
+        return total;
+    }
+
+    public double getVolumenOcupado(){
+        double total= 0.0;
+        for (Objeto objeto : this.getObjetos()) {
+            if(objeto.isDisponible()){
+                total+= objeto.getVolumen();
+            }
+        }
+        return total;
+    }
+
 }
