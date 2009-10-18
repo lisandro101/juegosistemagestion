f16d34405ec8682c373827f5c6a3d6df1ba67091
arielmartin9 <arielmartin9@8dbf1cc4-b889-11de-b017-a9a2c7051f87>
2009-10-18 19:49:16 +0000

    agregando clases y relaciones necessarias para la colonia de hormigas

---
+++ juegosistemagestion/src/juegosistemagestion/entidades/Hormiga.java
@@ -0,0 +1,45 @@
+
+
+package juegosistemagestion.entidades;
+
+import java.util.List;
+
+public class Hormiga {
+    private double feromonaInicial = 0.1;// es la probabilidad del objeto dividido la suma de las probabilidades de todos
+    private double factorEvaporacionFeromona = 0.5;
+    private double probAcumulada;
+    private double probNormal;
+    List<Objeto> camino;
+
+    public double getFeromonaInicial() {
+        return feromonaInicial;
+    }
+
+    public void setFeromonaInicial(double feromonaInicial) {
+        this.feromonaInicial = feromonaInicial;
+    }
+
+    public double getFactorEvaporacionFeromona() {
+        return factorEvaporacionFeromona;
+    }
+
+    public void setFactorEvaporacionFeromona(double factorEvaporacionFeromona) {
+        this.factorEvaporacionFeromona = factorEvaporacionFeromona;
+    }
+
+    public double getProbAcumulada() {
+        return probAcumulada;
+    }
+
+    public void setProbAcumulada(double probAcumulada) {
+        this.probAcumulada = probAcumulada;
+    }
+
+    public double getProbNormal() {
+        return probNormal;
+    }
+
+    public void setProbNormal(double probNormal) {
+        this.probNormal = probNormal;
+    }
+}
